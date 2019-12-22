253501ece35bd305fedeeb80fd2384168ccd2ec2
Masiu <ciszekmarcell@gmail.com>
2019-12-22 21:28:05 +0100

    buble preformence

--- interview/problemsolving/gradingStudents/index.js
+++ interview/problemsolving/gradingStudents/index.js
@@ -3,8 +3,17 @@
  * @param {number[]} grades
  */
 function gradingStudents(grades) {
-  const calulatedGrades = grades.filter(grade => grade >= 40);
   const multiplyByFive = x => x % 5 === 0;
+  const res = [];
+  for (let i = 0; i < grades.length; i++) {
+    // console.log(grades[i]);
+    console.log(multiplyByFive(grades[i]));
+    if (multiplyByFive(grades[i])) {
+      res.push(grades[i]);
+    }
+  }
+
+  console.log(res);
 }
 
-console.log(gradingStudents([4, 73, 67, 38, 33]));
+console.log(gradingStudents([4, 73, 67, 38, 33, 45]));
