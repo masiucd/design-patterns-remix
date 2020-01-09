9b35a42c115b5bdd16685096ac5e58beb1c40f5c
Masiu <ciszekmarcell@gmail.com>
2020-01-09 10:59:25 +0100

    div pair sum

---
+++ interview/findDigits/index.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * @param {number} n
+ * @returns {number}
+ */
+function findDigits(n) {
+  return n % ;
+}
+
+console.log(findDigits(2)); // 2
+console.log(findDigits(12)); // 3
---
+++ interview/problemsolving/jumpingOnClouds/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * @param {number[]} c
+ * @returns {number}
+ */
+function jumpingOnClouds(c) {
+  
+}
+
+// if 2 zeros in a row then jup one of the zeros
+// never 2 ones in a row
+// always a zero t the end
+
+const x = [0, 0, 0, 0, 1, 0]; // 3
+const y = [0, 0, 1, 0, 0, 1, 0]; // 4
+
+console.log(jumpingOnClouds(x));
