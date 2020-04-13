6ae8a6e50ccea34b2d7d840bd1e42b2b3cdebe5d
Masiu <ciszekmarcell@gmail.com>
2020-04-13 12:49:33 +0200

    birthday cake

---
+++ problemsolving/apples_and_oranges/index.js
@@ -0,0 +1,14 @@
+// When a fruit falls from its tree, it lands  units of distance from its tree of
+// origin along the -axis. A negative value of  means the fruit fell  units to the tree's
+// left, and a positive value of  means it falls  units to the tree's right.
+
+// s: integer, starting point of Sam's house location.
+// t: integer, ending location of Sam's house location.
+// a: integer, location of the Apple tree.
+// b: integer, location of the Orange tree.
+// apples: integer array, distances at which each apple falls from the tree.
+// oranges: integer array, distances at which each orange falls from the tree.
+
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+  //
+}
--- problemsolving/cats_and_mouse/index.js
+++ problemsolving/cats_and_mouse/index.js
@@ -9,9 +9,15 @@
  */
 function catsAndMouse(x, y, z) {
   let winningStr = '';
-  if (x > y && x < z) winningStr = 'Cat A';
-  if (y > x && y < z) winningStr = 'Cat B';
-  if (z > x && z < y) winningStr = 'Mouse C';
+  const diff1 = Math.abs(x - z);
+  const diff2 = Math.abs(y - z);
+  if (diff1 < diff2) {
+    winningStr = 'Cat A';
+  } else if (diff2 < diff1) {
+    winningStr = 'Cat B';
+  } else {
+    winningStr = 'Mouse C';
+  }
 
   return winningStr;
 }
