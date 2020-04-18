a92b0984e348628ce5a45e126dbfe35e2043dfd3
Masiu <ciszekmarcell@gmail.com>
2020-04-18 10:39:14 +0200

    new regex

---
+++ regex/char_range/index.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * @param {string} str
+ */
+const charRange = str => {
+  const re = /^[a-z][1-9][^a-z][^A-Z][A-Z]$/;
+  const re2 = /(^[a-z][1-9][^a-z][^A-Z][A-Z].*$)/;
+};
+
+console.log(charRange('h4CkRank')); // true
+console.log(charRange('q9$?WWe')); // true
+
+// The string's length is >= 5.
+// The first character must be a lowercase English alphabetic character.
+// The second character must be a positive digit. Note that we consider zero to be neither positive nor negative.
+// The third character must not be a lowercase English alphabetic character.
+// The fourth character must not be an uppercase English alphabetic character.
+// The fifth character must be an uppercase English alphabetic character.
