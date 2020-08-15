e060ce5376018844185b9a01439c79a4ca8d9060
Masiu <ciszekmarcell@gmail.com>
2020-08-15 12:24:33 +0200

    crud app

--- src/resolvers/MovieResolver.ts
+++ src/resolvers/MovieResolver.ts
@@ -1,15 +1,49 @@
-import { Arg, Int, Mutation, Query, Resolver } from "type-graphql";
+import {
+  Arg,
+  Field,
+  InputType,
+  Int,
+  Mutation,
+  Query,
+  Resolver,
+} from "type-graphql";
 import { Movie } from "../entity/Movie";
+
+@InputType()
+class MovieInput {
+  @Field(() => String)
+  title: string;
+
+  @Field(() => Int)
+  length: number;
+}
 
 @Resolver()
 export class MovieResolver {
-  @Mutation(() => Movie)
+  @Mutation(() => Boolean)
   async createMovie(
     @Arg("title") title: string,
     @Arg("length", () => Int) length: number,
   ) {
-    const newMovie = await Movie.create({ title, length }).save();
-    return newMovie;
+    await Movie.insert({ title, length });
+    return true;
+  }
+
+  @Mutation(() => [Movie])
+  async updateMovie(
+    @Arg("id", () => Int) id: number,
+    @Arg("options", () => MovieInput) options: MovieInput,
+  ) {
+    await Movie.update({ id }, options);
+
+    return await Movie.find;
+  }
+
+  @Mutation(() => Boolean)
+  async deleteMovie(@Arg("id", () => Int) id: number) {
+    await Movie.delete({ id });
+
+    return true;
   }
 
   @Query(() => [Movie])
