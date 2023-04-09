10fede891ea207a6742beefc207ba6ab9d2ac512
masiucd <ciszekmarcell@gmail.com>
2023-04-09 08:41:48 +0200

    JSON mod

---
+++ read_files_io/README.md
@@ -0,0 +1,6 @@
+# Read json and text files in Rust
+
+### Command to run
+
+1. cargo run json
+2. cargo run text
--- read_files_io/src/main.rs
+++ read_files_io/src/main.rs
@@ -1,96 +1,13 @@
-use std::fmt;
-
-use serde_derive::{ Deserialize, Serialize };
-use tabled::{ Table, Tabled };
-
 mod read_files;
-
-#[derive(Debug)]
-enum FoodType {
-    Main,
-    Side,
-    Drink,
-}
-
-impl fmt::Display for FoodType {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            FoodType::Main => write!(f, "Main"),
-            FoodType::Side => write!(f, "Side"),
-            FoodType::Drink => write!(f, "Drink"),
-        }
-    }
-}
-
-#[derive(Deserialize, Debug, Serialize, Tabled)]
-struct Food {
-    name: String,
-    price: f32,
-    description: String,
-    food_type: String, // TODO convert to enum
-}
-
-#[derive(Deserialize, Debug, Serialize, Tabled)]
-struct City {
-    name: String,
-    country: String,
-    population: u32,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct JsonData {
-    foods: Vec<Food>,
-    cities: Vec<City>,
-}
-
-impl City {
-    fn new(name: String, country: String, population: u32) -> Self {
-        Self {
-            name,
-            country,
-            population,
-        }
-    }
-}
-
-impl Food {
-    fn new(name: String, price: f32, description: String, food_type: String) -> Self {
-        Self {
-            name,
-            price,
-            description,
-            food_type,
-        }
-    }
-}
+mod read_json;
 
 fn main() {
     // get input path
-    // run cargo run <json or text>
     let input_path = std::env::args().nth(1).unwrap();
-    if input_path.trim() == "json" {
-        //read json file
-
-        let file = std::fs::read_to_string("data.json").unwrap();
-        let data: JsonData = serde_json::from_str(&file).unwrap();
-        let city_table = data.cities
-            .iter()
-            .map(|x| City::new(x.name.clone(), x.country.clone(), x.population))
-            .collect::<Vec<City>>();
-        println!("{}", Table::new(&city_table).to_string());
-
-        let food_table = data.foods
-            .iter()
-            .map(|x| Food::new(x.name.clone(), x.price, x.description.clone(), x.food_type.clone()))
-            .collect::<Vec<Food>>();
-
-        println!("{}", Table::new(&food_table).to_string());
-    } else if input_path.trim() == "text" {
-        //read text file
-    } else {
-        println!("Please provide a valid input path (json or text)");
+    // run cargo run <json or text>
+    match input_path.trim() {
+        "json" => read_json::io::run_read_json_app(),
+        "text" => read_files::io::run_read_file_mini_app(),
+        _ => println!("Please provide a valid input path (json or text)"),
     }
-
-    let x = FoodType::Main;
-    println!("{}", x)
 }
