dde221e99849a7f467170bb62165d8233c56416d
Marcell Ciszek Druzynski <ciszekmarcell@gmail.com>
2023-04-21 13:42:10 +0200

    Render table

--- personal_card/src/main.rs
+++ personal_card/src/main.rs
@@ -1,3 +1,28 @@
+use cli_table::{ format::{ Border, Justify }, Cell, Style, Table, CellStruct };
+
+fn get_headers() -> Vec<CellStruct> {
+    vec!["First Name".cell(), "Last Name".cell(), "Role".cell(), "Age".cell()]
+}
+
+fn get_body() -> Vec<Vec<CellStruct>> {
+    vec![
+        vec![
+            "Marcell".cell(),
+            "Ciszek Druzynski".cell(),
+            "Software developer".cell(),
+            (8).cell().justify(Justify::Right)
+        ]
+    ]
+}
+
 fn main() {
-    println!("Hello, world!");
+    let table = get_body()
+        .table()
+        .title(get_headers())
+        .bold(true)
+        .border(Border::builder().build());
+
+    let table_display = table.display().unwrap();
+
+    println!("{}", table_display);
 }
