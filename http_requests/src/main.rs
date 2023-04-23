d81137dc911311bf08b4b70c081206e6e8618eb3
masiucd <ciszekmarcell@gmail.com>
2023-04-23 18:37:07 +0200

    Get users

--- http_requests/src/main.rs
+++ http_requests/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+async fn get_users_as_text() -> Result<String, reqwest::Error> {
+    let users = reqwest::get("https://jsonplaceholder.typicode.com/users").await?.text().await?;
+    Ok(users)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let users = get_users_as_text().await?;
+    println!("users = {}", users);
+    Ok(())
 }
