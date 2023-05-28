169b1a3739c02d30826aca1be138ab80daa3547f
masiucd <ciszekmarcell@gmail.com>
2023-05-28 17:32:18 +0200

    Money converter

--- currency/target/debug/deps/currency-386c34afb32cec70.d
+++
@@ -1,5 +0,0 @@
-/Users/masiu_cd/projects/rust_apps/currency/target/debug/deps/currency-386c34afb32cec70.rmeta: src/main.rs
-
-/Users/masiu_cd/projects/rust_apps/currency/target/debug/deps/currency-386c34afb32cec70.d: src/main.rs
-
-src/main.rs:
--- currency/target/debug/deps/currency-468be1cd3d225b38.d
+++
@@ -1,5 +0,0 @@
-/Users/masiu_cd/projects/rust_apps/currency/target/debug/deps/currency-468be1cd3d225b38.rmeta: src/main.rs
-
-/Users/masiu_cd/projects/rust_apps/currency/target/debug/deps/currency-468be1cd3d225b38.d: src/main.rs
-
-src/main.rs:
