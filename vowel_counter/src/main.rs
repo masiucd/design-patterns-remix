5aa137140522ff0e17af8173ce497cf74f7409a1
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2023-05-31 12:15:00 +0200

    Vowel counter

--- vowel_counter/src/main.rs
+++ vowel_counter/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let mut user_input = String::from("");
+    println!("Enter a word: ");
+    std::io::stdin().read_line(&mut user_input).expect("Failed to read the input");
+    let vowels_count = count_vowels(&user_input.trim());
+    println!(
+        "The word you entered is: {} and the amount of vowels it contains is {}",
+        user_input,
+        vowels_count
+    );
+}
+
+const vowels: [char; 5] = ['a', 'e', 'i', 'o', 'u'];
+fn count_vowels(word: &str) -> i32 {
+    let mut count = 0;
+    for alpha in word.chars() {
+        if vowels.contains(&alpha) {
+            count += 1;
+        }
+    }
+    count
 }
