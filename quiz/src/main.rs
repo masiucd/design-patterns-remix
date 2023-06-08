af92a651a4c199b0cdeeaf0f1475796c69a12d77
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2023-06-08 12:00:39 +0200

    Quiz app

--- quiz/src/main.rs
+++ quiz/src/main.rs
@@ -25,12 +25,11 @@ struct UserAnswer {
     answer: String,
 }
 
-fn collect_answers(quiz: &Quiz) -> Result<Vec<UserAnswer>, io::Error> {
+fn collect_user_answers(quiz: &Quiz) -> Result<Vec<UserAnswer>, io::Error> {
     let mut user_answers: Vec<UserAnswer> = Vec::new();
     for q in quiz.questions.iter() {
         let mut user_answer = String::new();
         println!("{}", q.question);
-        println!("Enter your answer for question {}: ", q.id);
         io::stdin().read_line(&mut user_answer)?;
         user_answers.push(UserAnswer {
             id: q.id,
@@ -40,39 +39,29 @@ fn collect_answers(quiz: &Quiz) -> Result<Vec<UserAnswer>, io::Error> {
     Ok(user_answers)
 }
 
-fn main() -> Result<(), io::Error> {
-    // let mut user_answers: Vec<UserAnswer> = Vec::new();
+fn calculate_score(user_answers: &Vec<UserAnswer>, quiz: &Quiz) -> u8 {
+    let mut score = 0;
+    for a in user_answers.iter() {
+        let Answer { id: _, answer } = quiz.answers
+            .iter()
+            .find(|&x| x.id == a.id)
+            .unwrap();
+        if answer.to_lowercase() == a.answer.to_lowercase() {
+            println!("Correct!");
+            score += 1;
+        } else {
+            println!("Incorrect!");
+        }
+    }
+    score
+}
 
+fn main() -> Result<(), io::Error> {
     if let Ok(file) = std::fs::read_to_string("quiz.json") {
         let quiz: Quiz = serde_json::from_str(&file)?;
-        // for q in quiz.questions.iter() {
-        //     let mut user_answer = String::new();
-        //     println!("{}", q.question);
-        //     println!("Enter your answer for question {}: ", q.id);
-        //     io::stdin().read_line(&mut user_answer)?;
-        //     user_answers.push(UserAnswer {
-        //         id: q.id,
-        //         answer: user_answer.trim().to_string(),
-        //     });
-        // }
-
-        let user_answers = collect_answers(&quiz)?;
-
-        let mut score = 0;
-        for a in user_answers.iter() {
-            let Answer { id: _, answer } = quiz.answers
-                .iter()
-                .find(|&x| x.id == a.id)
-                .unwrap();
-            if answer.to_lowercase() == a.answer.to_lowercase() {
-                println!("Correct!");
-                score += 1;
-            } else {
-                println!("Incorrect!");
-            }
-        }
-        println!("Your score is: {} out of {}", score, quiz.questions.len());
+        let user_answers = collect_user_answers(&quiz)?;
+        let score = calculate_score(&user_answers, &quiz);
+        println!("Your score is: {}/{} possible", score, quiz.questions.len());
     }
-
     Ok(())
 }
