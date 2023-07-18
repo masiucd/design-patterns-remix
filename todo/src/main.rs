4c19badec05ec2f642d8b0beba0dca5495253ab3
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2023-07-18 16:29:11 +0200

    Options

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -12,8 +12,8 @@ struct Todo {
 }
 
 fn read_todos_from_json() -> Vec<Todo> {
-    let todos = std::fs::read_to_string("todos.json").unwrap();
-    let mut todos: Vec<Todo> = serde_json::from_str(&todos).unwrap();
+    let todos = std::fs::read_to_string("todos.json").unwrap_or_else(|_| String::from("[]"));
+    let todos: Vec<Todo> = serde_json::from_str(&todos).unwrap_or_else(|_| vec![]);
     todos
 }
 
@@ -32,16 +32,40 @@ fn build_table() -> (Vec<Todo>, Table) {
     (todos, table)
 }
 
+#[derive(Debug)]
+enum Option {
+    Add,
+    View,
+    Mark,
+    Remove,
+    Edit,
+    Exit,
+}
+
+impl std::fmt::Display for Option {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Option::Add => write!(f, "Add a new task"),
+            Option::View => write!(f, "View all tasks"),
+            Option::Mark => write!(f, "Mark a task as completed"),
+            Option::Remove => write!(f, "Remove a task"),
+            Option::Edit => write!(f, "Edit a task"),
+            Option::Exit => write!(f, "Exit"),
+        }
+    }
+}
+
 fn main() {
-    let options: Vec<&str> = vec![
-        "Add a nwe task",
-        "View all tasks",
-        "Mark a task as completed",
-        "Remove a task",
-        "Edit a task",
-        "Exit"
+    let options: Vec<Option> = vec![
+        Option::Add,
+        Option::View,
+        Option::Mark,
+        Option::Remove,
+        Option::Edit,
+        Option::Exit
     ];
-    let ans: Result<&str, InquireError> = Select::new(
+
+    let ans: Result<Option, InquireError> = Select::new(
         "Select what operation you would like to do",
         options
     ).prompt();
@@ -49,50 +73,62 @@ fn main() {
     match ans {
         Ok(ans) => {
             println!("You selected: {}", ans);
+            match ans {
+                Option::Add => {
+                    println!("Will add soon");
+                }
+                Option::View => {
+                    let (_, table) = build_table();
+                    println!("{}", table);
+                }
+                _ => {
+                    println!("Coming soon!");
+                }
+            }
         }
         Err(_) => println!("Error"),
     }
 
-    let todos = std::fs::read_to_string("todos.json").unwrap();
-    let mut todos: Vec<Todo> = serde_json::from_str(&todos).unwrap();
+    // let todos = std::fs::read_to_string("todos.json").unwrap();
+    // let mut todos: Vec<Todo> = serde_json::from_str(&todos).unwrap();
 
-    let mut table = Table::new();
+    // let mut table = Table::new();
 
-    table.set_header(vec!["id", "task", "completed"]);
+    // table.set_header(vec!["id", "task", "completed"]);
 
-    for todo in &todos {
-        table.add_row(
-            &vec![todo.id.to_string().as_str(), &todo.task, todo.completed.to_string().as_str()]
-        );
-    }
+    // for todo in &todos {
+    //     table.add_row(
+    //         &vec![todo.id.to_string().as_str(), &todo.task, todo.completed.to_string().as_str()]
+    //     );
+    // }
 
-    println!("{}", table);
+    // println!("{}", table);
 
-    let title = Text::new("Add the title of the task").prompt();
-    match title {
-        Ok(title) => {
-            let new_task = Todo {
-                id: (todos.len() as u8) + 1,
-                task: title,
-                completed: false,
-            };
-            todos.push(new_task);
-            let todos = serde_json::to_string(&todos).unwrap();
-            std::fs::write("todos.json", todos).unwrap();
-        }
-        Err(_) => println!("Error"),
-    }
+    // let title = Text::new("Add the title of the task").prompt();
+    // match title {
+    //     Ok(title) => {
+    //         let new_task = Todo {
+    //             id: (todos.len() as u8) + 1,
+    //             task: title,
+    //             completed: false,
+    //         };
+    //         todos.push(new_task);
+    //         let todos = serde_json::to_string(&todos).unwrap();
+    //         std::fs::write("todos.json", todos).unwrap();
+    //     }
+    //     Err(_) => println!("Error"),
+    // }
 
-    let todos = std::fs::read_to_string("todos.json").unwrap();
-    let todos: Vec<Todo> = serde_json::from_str(&todos).unwrap();
+    // let todos = std::fs::read_to_string("todos.json").unwrap();
+    // let todos: Vec<Todo> = serde_json::from_str(&todos).unwrap();
 
-    let mut table = Table::new();
+    // let mut table = Table::new();
 
-    table.set_header(vec!["id", "task", "completed"]);
-    for todo in &todos {
-        table.add_row(
-            &vec![todo.id.to_string().as_str(), &todo.task, todo.completed.to_string().as_str()]
-        );
-    }
-    println!("{}", table);
+    // table.set_header(vec!["id", "task", "completed"]);
+    // for todo in &todos {
+    //     table.add_row(
+    //         &vec![todo.id.to_string().as_str(), &todo.task, todo.completed.to_string().as_str()]
+    //     );
+    // }
+    // println!("{}", table);
 }
