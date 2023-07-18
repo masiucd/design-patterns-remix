be00c7b34dd6bbbfc250bc849cce28a822de9c1f
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2023-07-18 16:04:26 +0200

    Add a new task

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,4 +1,5 @@
 use comfy_table::Table;
+use inquire::Text;
 use serde::{ Serialize, Deserialize };
 
 use serde_json;
@@ -12,7 +13,7 @@ struct Todo {
 
 fn main() {
     let todos = std::fs::read_to_string("todos.json").unwrap();
-    let todos: Vec<Todo> = serde_json::from_str(&todos).unwrap();
+    let mut todos: Vec<Todo> = serde_json::from_str(&todos).unwrap();
 
     let mut table = Table::new();
 
@@ -24,5 +25,33 @@ fn main() {
         );
     }
 
+    println!("{}", table);
+
+    let title = Text::new("Add the title of the task").prompt();
+    match title {
+        Ok(title) => {
+            let new_task = Todo {
+                id: (todos.len() as u8) + 1,
+                task: title,
+                completed: false,
+            };
+            todos.push(new_task);
+            let todos = serde_json::to_string(&todos).unwrap();
+            std::fs::write("todos.json", todos).unwrap();
+        }
+        Err(_) => println!("Error"),
+    }
+
+    let todos = std::fs::read_to_string("todos.json").unwrap();
+    let todos: Vec<Todo> = serde_json::from_str(&todos).unwrap();
+
+    let mut table = Table::new();
+
+    table.set_header(vec!["id", "task", "completed"]);
+    for todo in &todos {
+        table.add_row(
+            &vec![todo.id.to_string().as_str(), &todo.task, todo.completed.to_string().as_str()]
+        );
+    }
     println!("{}", table);
 }
