2cd20669829112be756bb12acfc8bee0764e4849
marcell ciszek <ciszekmarcell@gmail.com>
2024-11-15 11:15:39 +0100

    Remove unused debug files and clean up target directory for read-files project

--- read-files/target/debug/deps/read_files-ae38bec752340e5f.d
+++
@@ -1,5 +0,0 @@
-/Users/dev-marcis/masiu-dev/rust-apps/read-files/target/debug/deps/libread_files-ae38bec752340e5f.rmeta: src/main.rs
-
-/Users/dev-marcis/masiu-dev/rust-apps/read-files/target/debug/deps/read_files-ae38bec752340e5f.d: src/main.rs
-
-src/main.rs:
--- read-files/target/debug/deps/read_files-de9459be0236503b.d
+++
@@ -1,5 +0,0 @@
-/Users/dev-marcis/masiu-dev/rust-apps/read-files/target/debug/deps/libread_files-de9459be0236503b.rmeta: src/main.rs
-
-/Users/dev-marcis/masiu-dev/rust-apps/read-files/target/debug/deps/read_files-de9459be0236503b.d: src/main.rs
-
-src/main.rs:
