f4b6610cab3033e1945d2f95f021f3fce3a66c9f
marcell ciszek <ciszekmarcell@gmail.com>
2024-11-15 11:36:21 +0100

    Refactor JSON structure and update read_json_file to use structured data types

--- read-files/src/main.rs
+++ read-files/src/main.rs
@@ -1,5 +1,7 @@
 use std::{ fs, io::{ self, Write } };
 
+use serde::{ Deserialize, Serialize };
+
 fn main() {
     let mut file_option = String::new();
     println!("Enter what file type you want to read: ");
@@ -34,16 +36,34 @@ fn read_text_file(file_name: &str) {
     }
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Student {
+    name: String,
+    age: u8,
+    pets: Vec<Pet>,
+    is_student: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Pet {
+    name: String,
+    pet_type: String,
+}
+
 fn read_json_file(file_name: &str) {
     let file = if file_name == "" { "j.json" } else { file_name };
     println!("Reading JSON file: {}", file);
     let json = fs::read_to_string(file);
     match json {
         Ok(j) => {
-            // parse json file
-            let parsed: serde_json::Value = serde_json::from_str(&j).expect("Failed to parse JSON");
-            println!("{}", j);
-            println!("{:?}", parsed);
+            let parsed: Student = serde_json::from_str(&j).expect("Failed to parse JSON");
+            println!("Student name: {}", parsed.name);
+            println!("Student age: {}", parsed.age);
+            println!("Is student: {}", parsed.is_student);
+            for pet in parsed.pets {
+                println!("\t Pet name: {}", pet.name);
+                println!("\t Pet type: {}", pet.pet_type);
+            }
         }
         Err(e) => println!("Error: {}", e),
     }
